6616e1cb380288aaa7ba8912c00cd2fb5ceeb39f
Emir <emiratesbp@gmail.com>
2015-03-23 15:37:10 +0100

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="1201 Piper Boulevard"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Naples, FL 34110";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=2395142229;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flow"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
